d8904f1cba09a19c840724a65dc8b422a9a27023
Milos Naskovic <milosnaskovic17@gmail.com>
2023-09-19 07:25:03 +0200

    feature: added api calls for 5 day weather forecast

--- src/api/weather/index.ts
+++ src/api/weather/index.ts
@@ -1,9 +1,29 @@
 import axios from '../axiosInstance';
 
-export const getWeatherInfoByCoords = (lat: number, lon: number) =>
-  axios.get(
+export const getWeatherInfoByCoords = (lat: number, lon: number) => {
+  return axios.get(
     `/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`,
   );
+};
 
-export const getWeatherInfoByCityName = (city: string) =>
-  axios.get(`/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`);
+export const getWeatherInfoByCityName = (city: string) => {
+  return axios.get(
+    `/weather?q=${encodeURIComponent(city)}&appid=${
+      process.env.REACT_APP_API_KEY
+    }`,
+  );
+};
+
+export const getTwoDayForecastByCoords = (lat: number, lon: number) => {
+  return axios.get(
+    `/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`,
+  );
+};
+
+export const getFiveDayForecastByCityName = (city: string) => {
+  return axios.get(
+    `/forecast?q=${encodeURIComponent(city)}&appid=${
+      process.env.REACT_APP_API_KEY
+    }`,
+  );
+};
