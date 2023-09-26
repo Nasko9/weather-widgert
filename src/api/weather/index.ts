8574abcfe16fe9194ead66c1451a462e57f3b44a
Milos Naskovic <milosnaskovic17@gmail.com>
2023-09-26 19:50:30 +0200

    fix: deleted functions for weather data fetching

--- src/api/weather/index.ts
+++ src/api/weather/index.ts
@@ -1,11 +1,5 @@
 import axios from '../axiosInstance';
 
-// export const getWeatherInfoByCoords = (lat: number, lon: number) => {
-//   return axios.get(
-//     `/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`,
-//   );
-// };
-
 export const getWeatherInfoByCityName = (city: string) => {
   return axios.get(
     `/weather?q=${encodeURIComponent(city)}&appid=${
@@ -13,12 +7,6 @@ export const getWeatherInfoByCityName = (city: string) => {
     }`,
   );
 };
-
-// export const getFiveDayForecastByCoords = (lat: number, lon: number) => {
-//   return axios.get(
-//     `/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`,
-//   );
-// };
 
 export const getFiveDayForecastByCityName = (city: string) => {
   return axios.get(
